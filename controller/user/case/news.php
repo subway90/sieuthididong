b443ed2e2c25749bd263d0ec18636275e43a73c5
subway90 <nguyenhieu3105@gmail.com>
2024-05-02 09:23:08 +0700

    phân trang NEWS / CATEGORY

--- controller/user/case/news.php
+++ controller/user/case/news.php
@@ -8,7 +8,7 @@ $keyword = $offsetLimit = "";
 if(isset($_GET['tim-kiem']) && $_GET['tim-kiem']){
     $keyword = $_GET['tim-kiem'];
     $search = true;
-    $listNews = getAllFieldByCustom('news','slug,title,dateCreate,idUser,imageTitle','title LIKE "%'.moveCharSpecial($keyword).'%" AND status = 1');
+    $countNews = getOneFieldByCustom('news','COUNT(id) quantity','title LIKE "%'.moveCharSpecial($keyword).'%" AND status = 1 ORDER BY dateCreate DESC ');
 }
 
 if(isset($arrayURL[1]) && $arrayURL[1]) {
@@ -16,14 +16,14 @@ if(isset($arrayURL[1]) && $arrayURL[1]) {
     #ALL NEWS
     if($arrayURL[1] === 'tat-ca') {
         $activeCate = 0;
-        $listNews = getAllFieldByCustom('news','slug,title,dateCreate,idUser,imageTitle','status = 1 ORDER BY dateCreate DESC '.$offsetLimit);
+        $countNews = getOneFieldByCustom('news','COUNT(id) quantity','status = 1 ORDER BY dateCreate DESC ');
     }else {
         #CATEGORY CASE
         for ($i=0; $i < count($listCate); $i++) { 
             extract($listCate[$i]);
             if(moveSlug($arrayURL[1]) == moveSlug(createSlug($name))) {
                 $title = $name;
-                $listNews = getAllFieldByCustom('news','slug,title,dateCreate,idUser,imageTitle','idCate ='.$id.' AND status = 1 ORDER BY dateCreate DESC '.$offsetLimit);
+                $countNews = getOneFieldByCustom('news','COUNT(id) quantity','idCate ='.$id.' AND status = 1 ORDER BY dateCreate DESC ');
                 $category = true;
                 $activeCate = $id;
                 break;
@@ -41,37 +41,39 @@ if(isset($arrayURL[1]) && $arrayURL[1]) {
             }else show404('user');
         }
     }
-
     #PAGINATION
-    if(isset($arrayURL[2]) && $arrayURL[2] && substr($arrayURL[2],0,6) === 'trang-') {
-        $maxPage = ceil(count($listNews)/LIMIT_NEWS_IN_PAGE);
-        $pageActive = substr($arrayURL[2],6);
+    if(!$detail) {
+        if(isset($arrayURL[2]) && $arrayURL[2] && substr($arrayURL[2],0,6) === 'trang-') $pageActive = substr($arrayURL[2],6);
+        else $pageActive = 1;
+        $maxPage = ceil(($countNews['quantity'])/LIMIT_NEWS_IN_PAGE);
         if(checkNumber($pageActive) || !$pageActive) {
             if($pageActive <= $maxPage) {
                 $offsetLimit = ' LIMIT '.(($pageActive-1) * LIMIT_NEWS_IN_PAGE ).','.( LIMIT_NEWS_IN_PAGE * $pageActive );
             }else show404('user');
         }else show404('user');
-    }else {
-        echo 'This case do not paging.... [CASE/NEWS] line 55';
-        exit;
     }
     
 }else header('Location: '.URL.'tin-tuc/tat-ca');
 
 #RENDER VIEW
+//echo $_SERVER['REQUEST_URI'];
+//LIST NEWS
+if($search) $listNews = getAllFieldByCustom('news','slug,title,dateCreate,idUser,imageTitle','title LIKE "%'.moveCharSpecial($keyword).'%" AND status = 1 ORDER BY dateCreate DESC '.$offsetLimit);
+else if($category) $listNews = getAllFieldByCustom('news','slug,title,dateCreate,idUser,imageTitle','idCate ='.$id.' AND status = 1 ORDER BY dateCreate DESC '.$offsetLimit);
+else $listNews = getAllFieldByCustom('news','slug,title,dateCreate,idUser,imageTitle','status = 1 ORDER BY dateCreate DESC '.$offsetLimit);
 
 //FILTER BY CATEGORY
-$showFilterCategoryNews = '<a href="'.URL.'tin-tuc/" class="btn btn-sm btn-outline-success me-1 mt-1 '.matchValue('active',0,$activeCate).'"><small>Tất cả</small></a>';
+$showFilterCategoryNews = '<a href="'.URL.'tin-tuc/tat-ca/trang-1" class="btn btn-sm btn-outline-success me-1 mt-1 '.matchValue('active',0,$activeCate).'"><small>Tất cả</small></a>';
 for ($i=0; $i < count($listCate); $i++) { 
     extract($listCate[$i]);
-    $showFilterCategoryNews .= '<a href="'.URL.'tin-tuc/'.createSlug($name).'" class="btn btn-sm btn-outline-success me-1 mt-1 '.matchValue('active',$id,$activeCate).'"><small>'.$name.'</small></a>';  
+    $showFilterCategoryNews .= '<a href="'.URL.'tin-tuc/'.createSlug($name).'/trang-1" class="btn btn-sm btn-outline-success me-1 mt-1 '.matchValue('active',$id,$activeCate).'"><small>'.$name.'</small></a>';  
 }
 //PAGING SHOW
-$showPagingNews = '<a href="'.URL.'tin-tuc/tat-ca/trang-'.($pageActive-1).'" class="btn btn-sm btn-outline-success  border-end-0 '.matchValue('disabled',1,$pageActive).'">Trước</a>';
-for ($i=1; $i <= $maxPage; $i++) { 
-    $showPagingNews .= '<a href="'.URL.'tin-tuc/tat-ca/trang-'.$i.'" class="btn btn-sm btn-outline-success border-end-0 '.matchValue('active',$i,$pageActive).'">'.$i.'</a>';
+if(!$detail) {
+    $showPagingNews = '<a href="'.URL.'tin-tuc/tat-ca/trang-'.($pageActive-1).'" class="btn btn-sm btn-outline-success  border-end-0 '.matchValue('disabled',1,$pageActive).'">Trước</a>';
+    for ($i=1; $i <= $maxPage; $i++) $showPagingNews .= '<a href="'.URL.'tin-tuc/tat-ca/trang-'.$i.'" class="btn btn-sm btn-outline-success border-end-0 '.matchValue('active',$i,$pageActive).'">'.$i.'</a>';
+    $showPagingNews .= '<a href="'.URL.'tin-tuc/tat-ca/trang-'.($pageActive+1).'" class="btn btn-sm btn-outline-success '.matchValue('disabled',$maxPage,$pageActive).'">Sau</a>';
 }
-$showPagingNews .= '<a href="'.URL.'tin-tuc/tat-ca/trang-'.($pageActive+1).'" class="btn btn-sm btn-outline-success '.matchValue('disabled',$maxPage,$pageActive).'">Sau</a>';
 //VIEW DETAIL
 if($detail) {
     require_once '../../view/user/header.php';
