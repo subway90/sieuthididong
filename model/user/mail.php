f2d85ec8e938d715b2054616edb3c8666a90c0e8
subway90 <nguyenhieu3105@gmail.com>
2024-06-15 17:02:35 +0700

    chỉnh sửa MAILER

--- model/user/mail.php
+++ model/user/mail.php
@@ -5,7 +5,7 @@ use PHPMailer\PHPMailer\Exception;
 require_once '../../publics/mailer/src/Exception.php';
 require_once '../../publics/mailer/src/PHPMailer.php';
 require_once '../../publics/mailer/src/SMTP.php';
-
+require_once '../../config/APP.php';
 
 function SendMail()
 {
@@ -14,14 +14,14 @@ function SendMail()
         //Cấu hình Server
         $mail->SMTPDebug = 2;
         $mail->isSMTP(); // Sử dụng SMTP để gửi mail
-        $mail->Host = '	mail.sieuthididong.shop'; // Server SMTP của gmail
+        $mail->Host = 'smtp.gmail.com'; // Server SMTP của gmail
         $mail->SMTPAuth = true; // Bật xác thực SMTP
-        $mail->Username = 'lienhe@sieuthididong.shop'; // Tài khoản email
-        $mail->Password = 'HongHa7979@'; // Mật khẩu ứng dụng ở bước 1 hoặc mật khẩu email
+        $mail->Username = 'mobilevutru@gmail.com'; // Tài khoản email
+        $mail->Password = ''; // Mật khẩu ứng dụng ở bước 1 hoặc mật khẩu email
         $mail->SMTPSecure = 'ssl'; // Mã hóa SSL
         $mail->Port = 465; // Cổng kết nối SMTP là 587 [Ưu tiền dùng hơn 465]
         //Cấu hình thông tin người gửi
-        $mail->setFrom('lienhe@sieuthididong.shop', 'sieuthididong.shop'); // Địa chỉ email và tên người gửi
+        $mail->setFrom('mobilevutru@gmail.com', 'sieuthididong.shop'); // Địa chỉ email và tên người gửi
         $mail->addAddress('nguyenhieu3105@gmail.com', 'USER'); // Địa chỉ mail và tên người nhận
         // Định dạng Form HTML
         $mail->isHTML(true);
